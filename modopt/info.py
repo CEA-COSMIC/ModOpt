abbb22df3085adbe7dd5a403c240863b7760d51f
Samuel Farrens <samuel.farrens@gmail.com>
2018-07-26 11:22:30 +0200

    updated minor version

--- modopt/info.py
+++ modopt/info.py
@@ -6,12 +6,12 @@ This module provides some basic information about the ModOpt package.
 
 :Author: Samuel Farrens <samuel.farrens@cea.fr>
 
-:Version: 1.1.5
+:Version: 1.1.6
 
 """
 
 # Package Version
-version_info = (1, 1, 5)
+version_info = (1, 1, 6)
 __version__ = '.'.join(str(c) for c in version_info)
 
 __about__ = ('ModOpt \n\n '
