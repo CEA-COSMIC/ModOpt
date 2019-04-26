b36290e6384f7fc5674c937342cfcf2c02472927
Samuel Farrens <samuel.farrens@gmail.com>
2019-04-26 11:59:55 +0200

    another bug fix

--- modopt/opt/algorithms.py
+++ modopt/opt/algorithms.py
@@ -65,7 +65,7 @@ class SetUp(Observable):
     """
 
     def __init__(self, metric_call_period=5, metrics={}, verbose=False,
-                 progress=True):
+                 progress=True, **kwargs):
 
         self.converge = False
         self.verbose = verbose
