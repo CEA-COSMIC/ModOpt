845bf4cbb212436798bd2af7cc7b832c043bcb55
lelguedd <loubna.el-gueddari@inria.fr>
2019-05-06 16:46:41 +0200

    Changing comments

--- modopt/opt/proximity.py
+++ modopt/opt/proximity.py
@@ -530,7 +530,7 @@ class OrderedWeightedL1Norm(ProximityParent):
 
         """
 
-        cost_val = np.sum(self.weights * np.sort(np.abs(args[]))[::-1])
+        cost_val = np.sum(self.weights * np.sort(np.abs(args[0]))[::-1])
 
         if 'verbose' in kwargs and kwargs['verbose']:
             print(' - OWL NORM (X):', cost_val)
