34ff1a89787266098980bf8e868b0dc616b13cef
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-07 11:34:06 +0100

    add algorithm test.

--- modopt/tests/test_algorithms.py
+++ modopt/tests/test_algorithms.py
@@ -10,9 +10,16 @@ This module contains unit tests for the modopt.opt module.
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 from modopt.opt import algorithms, cost, gradient, linear, proximity, reweight
-from pytest_cases import (case, fixture, fixture_ref, parametrize,
-                          parametrize_with_cases)
+from pytest_cases import (
+    case,
+    fixture,
+    fixture_ref,
+    lazy_value,
+    parametrize,
+    parametrize_with_cases,
+)
 
 from test_helpers import Dummy
 
@@ -23,7 +30,35 @@ except ImportError:
     SKLEARN_AVAILABLE = False
 
 
-idty = lambda x_val: x_val
+@fixture
+def idty():
+    """Identity function."""
+    return lambda x: x
+
+
+@fixture
+def reweight_op():
+    """Reweight operator."""
+    data3 = np.arange(9).reshape(3, 3).astype(float) + 1
+    return reweight.cwbReweight(data3)
+
+
+def build_kwargs(kwargs):
+    """Build the kwargs for each algorithm, replacing placeholder by true value.
+
+    Direct parametrization somehow is not working with pytest-xdist and pytest-cases.
+    """
+    update_value = {
+        "idty": lambda x: x,
+        "lin_idty": linear.Identity(),
+        "reweight_op": reweight.cwbReweight(
+            np.arange(9).reshape(3, 3).astype(float) + 1
+        ),
+    }
+    # update the value of the dict is possible.
+    for key in kwargs:
+        kwargs[key] = update_value.get(kwargs[key], kwargs[key])
+    return kwargs
 
 
 class AlgoCases:
@@ -31,16 +66,15 @@ class AlgoCases:
 
     data1 = np.arange(9).reshape(3, 3).astype(float)
     data2 = data1 + np.random.randn(*data1.shape) * 1e-6
-    data3 = np.arange(9).reshape(3, 3).astype(float) + 1
     max_iter = 20
 
     @parametrize(
         kwargs=[
-            {"beta_update": idty, "auto_iterate": False, "cost": None},
-            {"beta_update": idty},
+            {"beta_update": "idty", "auto_iterate": False, "cost": None},
+            {"beta_update": "idty"},
             {"cost": None, "lambda_update": None},
-            {"beta_update": idty, "a_cd": 3},
-            {"beta_update": idty, "r_lazy": 3, "p_lazy": 0.7, "q_lazy": 0.7},
+            {"beta_update": "idty", "a_cd": 3},
+            {"beta_update": "idty", "r_lazy": 3, "p_lazy": 0.7, "q_lazy": 0.7},
             {"restart_strategy": "adaptive", "xi_restart": 0.9},
             {
                 "restart_strategy": "greedy",
@@ -50,68 +84,73 @@ class AlgoCases:
             },
         ]
     )
-    def case_forward_backward(self, kwargs):
+    def case_forward_backward(self, kwargs, idty):
         """Forward Backward case."""
+        kwargs = build_kwargs(kwargs)
         algo = algorithms.ForwardBackward(
+            self.data1,
             grad=gradient.GradBasic(self.data1, idty, idty),
             prox=proximity.Positivity(),
             **kwargs,
         )
         if kwargs.get("auto_iterate", None) is False:
             algo.iterate(self.max_iter)
-        return algo
+        return algo, kwargs
 
     @parametrize(
         kwargs=[
             {
                 "cost": None,
                 "auto_iterate": False,
-                "gamma_update": idty,
-                "beta_update": idty,
+                "gamma_update": "idty",
+                "beta_update": "idty",
             },
-            {"gamma_update": idty, "lambda_update": idty},
+            {"gamma_update": "idty", "lambda_update": "idty"},
             {"cost": True},
             {"cost": True, "step_size": 2},
         ]
     )
-    def case_gen_forward_backward(self, kwargs):
+    def case_gen_forward_backward(self, kwargs, idty):
         """General FB setup."""
+        kwargs = build_kwargs(kwargs)
         grad_inst = gradient.GradBasic(self.data1, idty, idty)
         prox_inst = proximity.Positivity()
         prox_dual_inst = proximity.IdentityProx()
         if kwargs.get("cost", None) is True:
             kwargs["cost"] = cost.costObj([grad_inst, prox_inst, prox_dual_inst])
         algo = algorithms.GenForwardBackward(
+            self.data1,
             grad=grad_inst,
             prox_list=[prox_inst, prox_dual_inst],
             **kwargs,
         )
         if kwargs.get("auto_iterate", None) is False:
             algo.iterate(self.max_iter)
-        return algo
+        return algo, kwargs
 
     @parametrize(
         kwargs=[
             {
-                "sigma_dual": idty,
-                "tau_update": idty,
-                "rho_update": idty,
+                "sigma_dual": "idty",
+                "tau_update": "idty",
+                "rho_update": "idty",
                 "auto_iterate": False,
             },
             {
-                "sigma_dual": idty,
-                "tau_update": idty,
-                "rho_update": idty,
+                "sigma_dual": "idty",
+                "tau_update": "idty",
+                "rho_update": "idty",
             },
             {
-                "linear": linear.Identity(),
+                "linear": "lin_idty",
                 "cost": True,
-                "reweight": reweight.cwbReweight(data3),
+                "reweight": "reweight_op",
             },
         ]
     )
-    def case_condat(self, kwargs):
+    def case_condat(self, kwargs, idty):
         """Condat Vu Algorithm setup."""
+        kwargs = build_kwargs(kwargs)
         grad_inst = gradient.GradBasic(self.data1, idty, idty)
         prox_inst = proximity.Positivity()
         prox_dual_inst = proximity.IdentityProx()
@@ -128,10 +167,10 @@ class AlgoCases:
         )
         if kwargs.get("auto_iterate", None) is False:
             algo.iterate(self.max_iter)
-        return algo
+        return algo, kwargs
 
     @parametrize(kwargs=[{"auto_iterate": False, "cost": None}, {}])
-    def case_pogm(self, kwargs):
+    def case_pogm(self, kwargs, idty):
         """POGM setup."""
         grad_inst = gradient.GradBasic(self.data1, idty, idty)
         prox_inst = proximity.Positivity()
@@ -147,6 +186,7 @@ class AlgoCases:
 
         if kwargs.get("auto_iterate", None) is False:
             algo.iterate(self.max_iter)
+        return algo, kwargs
 
     @parametrize(
         GradDescent=[
@@ -158,7 +198,7 @@ class AlgoCases:
             algorithms.SAGAOptGradOpt,
         ]
     )
-    def case_grad(self, GradDescent):
+    def case_grad(self, GradDescent, idty):
         """Gradient Descent algorithm test."""
         grad_inst = gradient.GradBasic(self.data1, idty, idty)
         prox_inst = proximity.Positivity()
@@ -170,4 +210,15 @@ class AlgoCases:
             prox=prox_inst,
             cost=cost_inst,
         )
-        return algo
+        algo.iterate()
+        return algo, {}
+
+
+@parametrize_with_cases("algo, kwargs", cases=AlgoCases)
+def test_algo(algo, kwargs):
+    """Test algorithms."""
+    if kwargs.get("auto_iterate") is False:
+        # algo already run
+        npt.assert_almost_equal(algo.idx, AlgoCases.max_iter - 1)
+    else:
+        npt.assert_almost_equal(algo.x_final, AlgoCases.data1)
