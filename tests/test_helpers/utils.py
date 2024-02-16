a79113b03ddaae8fa786ec1584270cf558b68cb8
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-02-16 23:04:13 +0100

    fix tests setup

--- tests/test_helpers/utils.py
+++ tests/test_helpers/utils.py
@@ -12,12 +12,12 @@ def failparam(*args, raises=None):
     """Return a pytest parameterization that should raise an error."""
     if not issubclass(raises, Exception):
         raise ValueError("raises should be an expected Exception.")
-    return pytest.param(*args, marks=pytest.mark.raises(exception=raises))
+    return pytest.param(*args, marks=[pytest.mark.xfail(exception=raises)])
 
 
 def skipparam(*args, cond=True, reason=""):
     """Return a pytest parameterization that should be skip if cond is valid."""
-    return pytest.param(*args, marks=pytest.mark.skipif(cond, reason=reason))
+    return pytest.param(*args, marks=[pytest.mark.skipif(cond, reason=reason)])
 
 
 class Dummy:
